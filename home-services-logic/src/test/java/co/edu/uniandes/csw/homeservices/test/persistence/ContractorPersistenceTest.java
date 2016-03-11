c1d75a7b1baf3cd43a0dfe0e03ab5f86a91db923
jhon0010 <jhoning.soft@gmail.com>
2016-03-10 23:18:45 -0500

    t

--- home-services-logic/src/test/java/co/edu/uniandes/csw/homeservices/test/persistence/ContractorPersistenceTest.java
+++ home-services-logic/src/test/java/co/edu/uniandes/csw/homeservices/test/persistence/ContractorPersistenceTest.java
@@ -212,6 +212,7 @@ public class ContractorPersistenceTest {
         
             Assert.assertNotEquals("", skillName);
             Assert.assertTrue(resp.size() > 0);
+            
         } 
     }
 }
