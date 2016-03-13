458b27128a1678159ce26f958529843459b0acc7
Luis Sebastian <ls.talero21@uniandes.edu.co>
2016-03-13 16:13:15 -0500

    Ajuste WorkExperienceEntity

--- home-services-logic/src/main/java/co/edu/uniandes/csw/homeservices/entities/WorkExperienceEntity.java
+++ home-services-logic/src/main/java/co/edu/uniandes/csw/homeservices/entities/WorkExperienceEntity.java
@@ -3,6 +3,8 @@ package co.edu.uniandes.csw.homeservices.entities;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import co.edu.uniandes.csw.crud.spi.entity.BaseEntity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import uk.co.jemos.podam.common.PodamExclude;
 import javax.persistence.ManyToOne;
 
@@ -12,12 +14,18 @@ import javax.persistence.ManyToOne;
 @Entity
 public class WorkExperienceEntity extends BaseEntity implements Serializable {
 
+    
+    
     private String description;
     /*
         How many hour took this job
     */
     private Integer hours;
 
+    @JoinTable (name = "CONTRACTORENTITY_WORKEXPERIENCEENTITY", 
+            inverseJoinColumns = { @JoinColumn(name = "CONTRACTORENTITY_ID") },
+            joinColumns = { @JoinColumn(name = "WORKEXPERIENCES_ID")}
+    )
     @PodamExclude
     @ManyToOne
     private ContractorEntity contractor;
